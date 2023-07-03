d8092409754b21e0dad2c1d4584e5ad1dd31ad9b
Tales-1 <j.naz248@outlook.com>
2023-07-03 23:12:19 +0100

    push template files to repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PersonalDashboardAppProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
