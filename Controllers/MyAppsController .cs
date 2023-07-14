0abc55e22f6e1b039e32c0d3c89d561ee99c0f79
Tales-1 <j.naz248@outlook.com>
2023-07-14 23:15:01 +0100

    Finished navbar for dashboard.
    Added controller action and startup.cs file.

---
+++ Controllers/MyAppsController .cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalDashboardAppProj.Models;
+using System.Diagnostics;
+
+namespace PersonalDashboardAppProj.Controllers;
+
+public class MyAppsController : Controller
+{
+    private readonly ILogger<MyAppsController> _logger;
+
+    public MyAppsController(ILogger<MyAppsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
